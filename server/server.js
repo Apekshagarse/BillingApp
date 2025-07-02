a613b8e295ee874ec27a8a02b7fb006a5fe16deb
Apeksha <134759404+Apekshagarse@users.noreply.github.com>
2025-07-02 22:57:43 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -34,12 +34,5 @@ mongoose.connect(process.env.DATABASE)
   });
 
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
-
 // Export for Vercel
 module.exports = app;
